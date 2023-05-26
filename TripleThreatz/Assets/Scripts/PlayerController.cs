210c2d6e4118594955e43c4072d4142c0e6d2888
NawtiGranni <Nickyy2006@outlook.com>
2023-05-26 08:43:30 -0700

    i made xchanghed to the game
    i made changes to the game

--- TripleThreatz/Assets/Scripts/PlayerController.cs
+++ TripleThreatz/Assets/Scripts/PlayerController.cs
@@ -1,20 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
+
+
+
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject projectilePrefab;
+    public float moveSpeed = 5f;
+    public float turnSpeed = 10f;
+    public float jumpForce = 5f;
+    private bool isJumping = false;
+    private Rigidbody rb;
 
-    // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
+        Vector3 movement = new Vector3(horizontal, 0f, vertical) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement, Space.Self);
+
+        // Rotation
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, transform.position);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Quaternion targetRotation = Quaternion.LookRotation(point - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+        }
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isJumping = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
     }
 }
